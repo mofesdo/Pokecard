b72c2fa5602d523bc209fdfa51a22dae4c2e2eeb
Modesto <mofesdo.dev@gmail.com>
2025-07-29 00:29:45 -0700

    feat: implement card creation functionality with modal and form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import CardModal from "./components/CardModal/CardModal";
 
 function App() {
   const [activeModal, setActiveModal] = useState("");
 
+  const handleCreateCardClick = () => {
+    setActiveModal("card");
+  };
+  const handleCardSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log("Card submitted");
+    setActiveModal(""); // Close the modal after submission
+  };
+
+  const closeActiveModal = () => {
+    setActiveModal("");
+  };
+
   return (
     <>
-    <button className='card__btn'>Create Catch Card</button>
-      <div className='card'>
-        
-      </div>
+      <button className="card__btn" type="button" onClick={handleCreateCardClick}>Create Catch Card</button>
+      <div className="card"></div>
+      <CardModal
+        onClose={closeActiveModal}
+        isOpen={activeModal === "card"}
+        onSubmit={handleCardSubmit}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/CardModal/CardModal.jsx
+++ src/components/CardModal/CardModal.jsx
@@ -3,6 +3,11 @@ import { useEffect, useState } from "react";
 
 function CardModal({ isOpen, onClose, onSubmit }) {
   const [activeModal, setActiveModal] = useState("");
+  const [name, setName] = useState("");
+
+    const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
 
   useEffect(() => {
     if (isOpen) {
@@ -22,8 +27,21 @@ function CardModal({ isOpen, onClose, onSubmit }) {
       onSubmit={onSubmit}
     >
       <div className="card__form">
-        {/* Form fields go here */}
+        <label htmlFor="login_email" className="modal__label">
+          Name
+          <input
+            type="text"
+            className="modal__input"
+            id="pokemon_name"
+            placeholder="Name"
+            onChange={handleNameChange}
+            value={name}
+            required
+          />
+        </label>
       </div>
     </ModalWithForm>
   );
 }
+
+export default CardModal;
